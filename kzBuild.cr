cc248068ed8af13829788d08ce7901eb9f01425f
Nilly (iago) TheL0L <97253814+NillyTheL0L@users.noreply.github.com>
2022-03-31 19:22:30 -0300

    Add files via upload

--- kzBuild.cr
+++ kzBuild.cr
@@ -4,7 +4,7 @@ system( "sudo apt install ark unrar unzip lzma bzip2" )
 puts "==> Downloading source...."
 system( "wget -P ~/ https://download848.mediafire.com/usfpq98cqn8g/3rzzuyn9bje9ey8/crzip" )
 puts "==> Giving Permissions...."
-system( "chmod +x ~/crzip" )
+system( "chmod +x ~/katanazip" )
 puts "==> Moving to /usr/bin"
-system ( "sudo mv ~/crzip /usr/bin" )
+system ( "sudo mv ~/katanazip /usr/bin" )
 puts "===> App installed with sucess now type katanazip -h"
